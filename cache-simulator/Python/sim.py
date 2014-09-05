c9dfb9f6c791dee411a15a998892b75448db6078
SunnyRaj <sundarcs@gwu.edu>
2014-09-05 19:17:36 -0400

    Implement StaticLRU split algorithm

--- cache-simulator/Python/sim.py
+++ cache-simulator/Python/sim.py
@@ -11,45 +11,12 @@ import cache
 # import pdb
 
 
-class IndexedOrderedDict(OrderedDict):
-
-    """
-    This is a custom class that combines the properties of
-    Dictionary, deque and counter. Dictionary + deque = OrderedDict
-    is a good data structure for implementing a LRU cache. When items
-    are added, it always adds it at the end of the Dictionary and items
-    can be popped from the front or arbitrarily.
-
-    This is combined further with a Counter, which increases the value of
-    a given keyword (in our case, the disk_id) by 1 when it is inserted and
-    decrease by 1 when deleted.
-
-    This saves us from a lot of trouble using a separate datastructure to
-    store it and making the program ugly with a ton of if else conditionals.
-    """
-
-    def __init__(self, key=lambda k: k, *args, **kwargs):
-        self.counter = Counter()
-        self.key_transform = key
-        super(IndexedOrderedDict, self).__init__(*args, **kwargs)
-
-    def __delitem__(self, key):
-        super(IndexedOrderedDict, self).__delitem__(key)
-        self.counter[self.key_transform(key)] -= 1
-
-    def __setitem__(self, key, value):
-        if key not in self:
-            self.counter[self.key_transform(key)] += 1
-
-        super(IndexedOrderedDict, self).__setitem__(key, value)
-
-
 class Sim(object):
 
     def __init__(self, filename=None, blocksize=8, cachesize=80):
-        self.EVICT_POLICY = "weightedLRU"
+        self.EVICT_POLICY = "staticLRU"
         # This will be the actual cache
-        self.ssd = IndexedOrderedDict(key=lambda k: k[1])
+        self.ssd = defaultdict(lambda: OrderedDict())
         self.filename = filename
         self.blocksize = blocksize
         self.cachesize = cachesize
@@ -71,67 +38,60 @@ class Sim(object):
             "evictions"	: 0
         }
 
-    def checkFreeSpace(self, UUID):
-        if (len(self.ssd) < self.maxsize):
+    def checkFreeSpace(self, disk_id):
+        if (len(self.ssd[disk_id]) < self.weight[disk_id]):
             if self.EVICT_POLICY == "staticLRU":
-                return self.ssd.counter[UUID[1]] < self.weight[UUID[1]]
+                return len(self.ssd[disk_id]) < self.weight[disk_id]
             else:  # Both global and weighted LRU care only about maxsize
                 return True
         return False
 
-    def sim_write(self, UUID):
-        pass
-
-    def sim_read(self, UUID):
-        if (UUID in self.ssd):
-            cache_contents = self.ssd.pop(UUID)
-            self.ssd[UUID] = cache_contents
-            self.ssd[UUID].set_lru()
+    def sim_read(self, disk_id, block_address):
+        if (block_address in self.ssd[disk_id]):
+            cache_contents = self.ssd[disk_id].pop(block_address)
+            self.ssd[disk_id][block_address] = cache_contents
+            self.ssd[disk_id][block_address].set_lru()
             self.state["hits"] += 1
-            self.stats[UUID[1], "hits"] += 1
+            self.stats[disk_id, "hits"] += 1
         else:
-            self.insert(UUID)
+            self.insert(disk_id, block_address)
             self.state["misses"] += 1
-            self.stats[UUID[1], "misses"] += 1
+            self.stats[disk_id, "misses"] += 1
 
-    def insert(self, UUID):
+    def insert(self, disk_id, block_address):
         new_cache_block = cache.Cache()
-        if self.checkFreeSpace(UUID):
-            self.ssd[UUID] = new_cache_block
-            self.ssd[UUID].set_lru()
+        if self.checkFreeSpace(disk_id):
+            self.ssd[disk_id][block_address] = new_cache_block
+            self.ssd[disk_id][block_address].set_lru()
         else:
-            self.evictItem(UUID)
-            self.ssd[UUID] = new_cache_block
-            self.ssd[UUID].set_lru()
+            self.evictItem(disk_id, block_address)
+            self.ssd[disk_id][block_address] = new_cache_block
+            self.ssd[disk_id][block_address].set_lru()
             self.state["evictions"] += 1
-            self.stats[UUID[1], "evictions"] += 1
+            self.stats[disk_id, "evictions"] += 1
 
-    def evictItem(self, UUID):
+    def evictItem(self, disk_id, block_address):
         if self.EVICT_POLICY == "weightedLRU":
-            delta = Counter(self.ssd.counter) - Counter(self.weight)
+            delta = Counter(
+                self.ssd[block_address].counter) - Counter(self.weight)
             # Negative values are ignored in the above expression.
             # We don't need them. To not ignore use:
-            # delta =  k: self.ssd.counter.get(k, 0) - self.weight.get(k, 0)
-            # for k in set(self.ssd.counter) & set(self.weight) }
+            # delta = k: self.ssd[block_address].counter.get(k
+            # , 0) - self.weight.get(k, 0) for k in set(self.ssd[
+            # block_address].counter) & set(self.weight) }
             try:
                 id_to_be_evicted = max(delta.iteritems(), key=itemgetter(1))[0]
             except ValueError:
                 # If all items are exactly equal to their weight,
                 # delta would be an empty sequence, hence ValueError.
-                self.ssd.popitem(last=False)
+                self.ssd[disk_id].popitem(last=False)
             else:
-                for item_to_be_evicted in self.ssd.keys():
-                    if item_to_be_evicted[1] == id_to_be_evicted:
-                        self.ssd.pop(item_to_be_evicted)
-                        break
+                self.ssd[id_to_be_evicted].popitem(last=False)
         elif self.EVICT_POLICY == "staticLRU":
-            # pop the first element that matches with the disk id
-            for item_to_be_evicted in self.ssd.keys():
-                if item_to_be_evicted[1] == UUID[1]:
-                    self.ssd.pop(item_to_be_evicted)
-                    break
+            self.ssd[disk_id].popitem(last=False)
         else:
-            self.ssd.popitem(last=False)
+                # TO-DO: Code for global LRU condition
+            pass
 
     def delete(self):
         self.resetCache()
@@ -153,11 +113,7 @@ class Sim(object):
                 for item in csv.reader(trace, delimiter=','):
                     operation, block_address, disk_id = item[
                         0], int(item[1], 0), int(item[2], 0)
-                    UUID = (block_address, disk_id)
-                    if operation == 'r':
-                        self.sim_read(UUID)
-                    else:
-                        self.sim_write(UUID)
+                    self.sim_read(disk_id, block_address)
                     # print self.ssd.keys()
                     # pdb.set_trace()
             self.print_stats()
