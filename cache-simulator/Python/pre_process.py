f6946b349a386edb660b725b673e4d101fd3c39e
SunnyRaj <sundarcs@gwu.edu>
2014-09-19 18:55:32 -0400

    Change filetime to epoch time

---
+++ cache-simulator/Python/pre_process.py
@@ -0,0 +1,25 @@
+import os
+import csv
+
+WINDOWS_TICK = 10000000
+SEC_TO_UNIX_EPOCH = 11644473600
+
+
+def WindowsTickToUnixSeconds(windowsTicks):
+    return (windowsTicks / WINDOWS_TICK - SEC_TO_UNIX_EPOCH)
+
+with open(os.path.join("traces/MSR-Cambridge/web", "pre-processed.csv"), "wb") as wr:
+    sentinal = 0
+    writr = csv.writer(wr, delimiter=',')
+    with open(os.path.join("traces/MSR-Cambridge/web", "exp.csv"), "rb") as trace:
+        for item in csv.reader(trace, delimiter=','):
+            if sentinal == 0:
+                sentinal = WindowsTickToUnixSeconds(int(item[0]))
+            time_of_access = WindowsTickToUnixSeconds(int(item[0])) - sentinal
+            hostname = item[1]
+            disk_id = int(item[2])
+            operation = item[3]
+            block_address = item[4]
+            read_size = int(item[5])
+            response_time = int(item[6])
+            writr.writerow([time_of_access, hostname, disk_id, operation, block_address, read_size, response_time])
