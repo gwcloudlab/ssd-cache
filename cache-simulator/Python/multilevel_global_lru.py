e88b0c8b34839594d70194802b031d9d9c9e116a
SunnyRaj <sundarcs@gwu.edu>
2015-02-11 22:46:25 -0500

    Fix stats calculation

--- cache-simulator/Python/multilevel_global_lru.py
+++ cache-simulator/Python/multilevel_global_lru.py
@@ -10,8 +10,8 @@ class Multilevel_global_lru(Cache):
     def __init__(self, no_of_vms):
         Cache.__init__(self)
         self.no_of_vms = no_of_vms
-        self.maxsize_pcie_ssd = 1000
-        self.maxsize_ssd = 1000
+        self.maxsize_pcie_ssd = 10000
+        self.maxsize_ssd = 100000
         self.pcie_ssd = OrderedDict()
         self.ssd = OrderedDict()
         self.ri = defaultdict(lambda: -1)
@@ -37,32 +37,32 @@ class Multilevel_global_lru(Cache):
                 self.unique_blocks[x] = hyperll
             pprint.pprint(dict(self.normalized_intensities))
 
-        if self.normalized_intensities[disk_id] > 0.1:
+        if self.normalized_intensities[disk_id] > 0.5:
             if (UUID in self.pcie_ssd):
                 cache_contents = self.pcie_ssd.pop(UUID)
                 self.pcie_ssd[UUID] = cache_contents
                 self.pcie_ssd[UUID].set_lru()
-                self.stats[disk_id, "hits"] += 1
+                self.stats[disk_id, "pcie_hits"] += 1
             else:
                 new_cache_block = Cache_entry()
                 if len(self.pcie_ssd) >= self.maxsize_pcie_ssd:
                     self.pcie_ssd.popitem(last=False)
-                    self.stats[disk_id, "evictions"] += 1
+                    self.stats[disk_id, "pcie_evictions"] += 1
                 self.pcie_ssd[UUID] = new_cache_block
-                self.stats[disk_id, "misses"] += 1
+                self.stats[disk_id, "pcie_misses"] += 1
         else:
             if (UUID in self.ssd):
                 cache_contents = self.ssd.pop(UUID)
                 self.ssd[UUID] = cache_contents
                 self.ssd[UUID].set_lru()
-                self.stats[disk_id, "hits"] += 1
+                self.stats[disk_id, "ssd_hits"] += 1
             else:
                 new_cache_block = Cache_entry()
                 if len(self.ssd) >= self.maxsize_ssd:
                     self.ssd.popitem(last=False)
-                    self.stats[disk_id, "evictions"] += 1
+                    self.stats[disk_id, "ssd_evictions"] += 1
                 self.ssd[UUID] = new_cache_block
-                self.stats[disk_id, "misses"] += 1
+                self.stats[disk_id, "ssd_misses"] += 1
 
     def calculate_reuse_intensity(self):
         self.normalized_intensities = defaultdict(lambda: 0)
