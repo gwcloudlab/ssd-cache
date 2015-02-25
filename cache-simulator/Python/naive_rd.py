0156a1a0283ebc6274e555603bb9d5f92578e1b5
Sunny Raj <sundarcs@gwu.edu>
2015-02-25 16:43:17 +0000

    Fix variable name reference errors

--- cache-simulator/Python/naive_rd.py
+++ cache-simulator/Python/naive_rd.py
@@ -4,8 +4,8 @@ from collections import OrderedDict
 
 class Naive_rd():
     def __init__(self):
-        self.rd = dict(OrderedDict)
-        self.rd_list = defaultdict(lambda: list)
+        self.rd = defaultdict(OrderedDict)
+        self.rd_list = defaultdict(list)
         self.rd_size_lookup = defaultdict(lambda: 0)
 
     def calculate_rd(self, disk_id, block_address):
@@ -13,10 +13,11 @@ class Naive_rd():
             indx = self.rd_list[disk_id].index(block_address)
             self.rd_list[disk_id].pop(indx)
             self.rd_list[disk_id].append(block_address)
-            no_of_entries = self.size_lookup[disk_id]
+            no_of_entries = self.rd_size_lookup[disk_id]
             rd_value = no_of_entries - indx + 1
         else:
             rd_value = -1
-            self.size_lookup[disk_id] += 1
+            self.rd_list[disk_id].append(block_address)
+            self.rd_size_lookup[disk_id] += 1
 
         self.rd[disk_id][block_address] = rd_value
