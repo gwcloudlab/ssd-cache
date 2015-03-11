1aeb354c32b4388fbeaec52d248b6bde16440eb1
Sunny Raj <sundarcs@gwu.edu>
2015-03-11 18:32:24 +0000

    Change datastructure

--- cache-simulator/Python/cs_rd.py
+++ cache-simulator/Python/cs_rd.py
@@ -6,33 +6,30 @@ from hyperloglog import HyperLogLog
 class Cs_rd():
     def __init__(self, no_of_vms):
         self.no_of_vms = no_of_vms
-        self.rd_list = defaultdict(lambda: [0])
-        self.deltaX = defaultdict(lambda: [1])
-        self.deltaY = defaultdict(lambda: [0])
+        self.rd_list = defaultdict(list)
+        self.deltaX = []
+        self.deltaY = []
+        self.current_column = []
+        self.previous_column = []
         self.rd_array = defaultdict(SortedList)
-        self.unique_blocks = defaultdict()
-        self.trace = defaultdict(list)
-        for x in xrange(self.no_of_vms):
-            hyperll = HyperLogLog(0.01)
-            self.unique_blocks[x] = hyperll
 
     def calculate_rd(self, disk_id, block_address):
         self.calculate_unique_matrix(disk_id, block_address)
         self.extract_last_two_columns(disk_id)
-        self.calculate_deltaX(disk_id)
-        self.calculate_deltaY(disk_id)
+        self.calculate_deltaX()
+        self.calculate_deltaY()
         self.calculate_rd_values(disk_id)
 
     def calculate_unique_matrix(self, disk_id, block_address):
         hyperll = HyperLogLog(0.01)
         self.rd_list[disk_id].append(hyperll)
-        for item in self.rd_list[disk_id].itervalues():
-            item.append(block_address)
+        for item in self.rd_list[disk_id]:
+            item.add(str(block_address))
 
     def extract_last_two_columns(self, disk_id):
         self.previous_column = self.current_column[:]
         self.current_column = []  # May be we can just update/reuse
-        for item in self.rd_list[disk_id].itervalues():
+        for item in self.rd_list[disk_id]:
             self.current_column.append(len(item))
 
     def calculate_deltaX(self):
@@ -49,7 +46,7 @@ class Cs_rd():
     def calculate_rd_values(self, disk_id):
         for indx in xrange(len(self.deltaY)):
             if self.deltaY[indx] > 0:
-                rd = self.current_list[indx]
+                rd = self.current_column[indx]
                 self.rd_array[disk_id].add(rd)
                 break  # Assuming there will only be one value >1
 
