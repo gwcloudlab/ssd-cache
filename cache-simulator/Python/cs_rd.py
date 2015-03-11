f2d5c9490b3c08b451653d3d79ab2206e2c78683
Sunny Raj <sundarcs@gwu.edu>
2015-03-11 17:00:48 +0000

    WIP: Add original method of counter stack

--- cache-simulator/Python/cs_rd.py
+++ cache-simulator/Python/cs_rd.py
@@ -11,35 +11,47 @@ class Cs_rd():
         self.deltaY = defaultdict(lambda: [0])
         self.rd_array = defaultdict(SortedList)
         self.unique_blocks = defaultdict()
+        self.trace = defaultdict(list)
         for x in xrange(self.no_of_vms):
             hyperll = HyperLogLog(0.01)
             self.unique_blocks[x] = hyperll
 
     def calculate_rd(self, disk_id, block_address):
-        unique_count = self.calculate_unique_elements(disk_id, block_address)
-        self.rd_list[disk_id].append(unique_count)
+        self.calculate_unique_matrix(disk_id, block_address)
+        self.extract_last_two_columns(disk_id)
         self.calculate_deltaX(disk_id)
         self.calculate_deltaY(disk_id)
         self.calculate_rd_values(disk_id)
 
-    def calculate_unique_elements(self, disk_id, block_address):
-        self.unique_blocks[disk_id].add(str(block_address))
-        return len(self.unique_blocks[disk_id])
+    def calculate_unique_matrix(self, disk_id, block_address):
+        hyperll = HyperLogLog(0.01)
+        self.rd_list[disk_id].append(hyperll)
+        for item in self.rd_list[disk_id].itervalues():
+            item.append(block_address)
 
-    def calculate_deltaX(self, disk_id):
-        last_element = self.rd_list[disk_id][-1] - self.rd_list[disk_id][-2]
-        self.deltaX[disk_id].append(last_element)
+    def extract_last_two_columns(self, disk_id):
+        self.previous_column = self.current_column[:]
+        self.current_column = []  # May be we can just update/reuse
+        for item in self.rd_list[disk_id].itervalues():
+            self.current_column.append(len(item))
 
-    def calculate_deltaY(self, disk_id):
-        last_element = self.deltaX[disk_id][-2] - self.deltaX[disk_id][-1]
-        self.deltaY[disk_id].append(last_element)
+    def calculate_deltaX(self):
+        self.deltaX = [x - y for x, y in
+                       zip(self.current_column, self.previous_column)]
+        # To compensate for the shorter current_column len.
+        self.deltaX.append(1)
+
+    def calculate_deltaY(self):
+        for val in xrange(len(self.deltaX)-1, 0, -1):
+            self.deltaY.append(self.deltaX[val] - self.deltaX[val-1])
+            # We can stop when we see a value >1
 
     def calculate_rd_values(self, disk_id):
-        if (self.deltaY[disk_id][-1] != 0):
-            self.rd_array[disk_id].add(self.rd_list[disk_id][-1])
-        else:
-            self.rd_array[disk_id].add(-1)
-
+        for indx in xrange(len(self.deltaY)):
+            if self.deltaY[indx] > 0:
+                rd = self.current_list[indx]
+                self.rd_array[disk_id].add(rd)
+                break  # Assuming there will only be one value >1
 
     def get_rd_values(self):
         return self.rd_array
