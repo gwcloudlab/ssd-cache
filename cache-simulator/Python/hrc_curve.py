519d40329313b34759f57ff69e11b5d7f299728e
SunnyRaj <sundarcs@gwu.edu>
2015-03-17 17:58:21 -0400

    Change ecdf calculation

--- cache-simulator/Python/hrc_curve.py
+++ cache-simulator/Python/hrc_curve.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from statsmodels import api as sm
 import matplotlib.pyplot as plt
 from itertools import cycle
 import numpy as np
@@ -6,18 +7,27 @@ from random import randint
 
 
 def compute_HRC(alg_name, rd_dict):
-    """
-    http://stackoverflow.com/questions/3209362/
-    how-to-plot-empirical-cdf-in-matplotlib-in-python
-    """
     lines = ['-', '--', '-.', ':']
     linecycler = cycle(lines)
 
     for disk in rd_dict.iterkeys():
         sorted_array = np.sort(rd_dict[disk][:])
-        yvals = np.arange(len(sorted_array))/float(len(sorted_array))
 
-        plt.plot(sorted_array, yvals,
+        # find the second largest element in the sorted array.
+        # ex. sorted_array = [1,2,3,3,3,4,1000,1000,...]
+        # We want that 1000 to be included in the cdf but not
+        # in the graph.
+        largest_element = sorted_array[-1]
+        for element in reversed(sorted_array):
+            if element < largest_element:
+                second_largest = element
+                break
+
+        ecdf = sm.distributions.ECDF(sorted_array)
+        x_vals = np.linspace(sorted_array[0], second_largest, second_largest)
+        y_vals = ecdf(x_vals)
+
+        plt.plot(x_vals, y_vals,
                  next(linecycler),
                  linewidth = 2.0,
                  label=alg_name + " disk: " + str(disk))
@@ -32,7 +42,9 @@ def draw_cdf():
     frame = legend.get_frame()
     frame.set_facecolor('0.90')
     plt.grid(True)
+    plt.xlim([0, 800])
     plt.show()
+    # plt.savefig('mattson_vs_counterstack.png')
 
 """
 def main():
