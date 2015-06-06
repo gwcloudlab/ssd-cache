92d55b1d62c3c3c4387b4446b8e811b27b26f84f
Sunny Raj <sundarcs@gwu.edu>
2015-06-06 02:18:34 -0400

    Convert MR trace to MSR format

---
+++ cache-simulator/Python/MRtoMSRformat.py
@@ -0,0 +1,22 @@
+import sys
+import csv
+
+for arg in sys.argv[1:]:
+    filename = arg
+    print filename
+    name, ext = filename.split('.')
+    output_filename = name + ".csv"
+
+with open(output_filename, "wb") as wr:
+    writr = csv.writer(wr, delimiter=',')
+    with open(filename, "rb") as trace:
+        for item in csv.reader(trace, delimiter=" "):
+            time_of_access = int(float(item[0]))
+            block_address = int(item[1])
+            read_size = int(item[2])
+            hostname = "mr"
+            disk_id = 5 # Should we change this???
+            operation = "Read"
+            response_time = 0
+            writr.writerow([time_of_access, hostname, disk_id, operation, block_address, read_size, response_time])
+
