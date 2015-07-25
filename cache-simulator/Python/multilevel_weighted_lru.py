1f9e6df574006188ccb598a36dcac2c78648f4c1
Sunny Raj <sundarcs@gwu.edu>
2015-07-25 18:22:11 -0400

    Change write to ammend on logs

--- cache-simulator/Python/multilevel_weighted_lru.py
+++ cache-simulator/Python/multilevel_weighted_lru.py
@@ -38,7 +38,7 @@ class Multilevel_weighted_lru(Cache):
             hyperll = HyperLogLog(0.01)
             self.unique_blocks[vm] = hyperll
         if __debug__:
-            with open('log/detailed_stats.log', 'w') as out_file:
+            with open('log/detailed_stats.log', 'a') as out_file:
                 out_file.write('Algorithm,Multi-level-weighted-LRU\n')
                 out_file.write('CurrentTime,' + str(datetime.now()) + '\n')
 
