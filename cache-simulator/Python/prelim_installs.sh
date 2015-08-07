cfae53807ddd9cb5f2697c73c03e136f2d3563d1
Sunny Raj <sundarcs@gwu.edu>
2015-08-07 16:12:57 -0400

    Inital installations on a new machine

---
+++ cache-simulator/Python/prelim_installs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo apt-get install -y python-dev python-pip  python-setuptools python-numpy python-scipy python-flake8 libfreetype6-dev libxft-dev libpng-dev
+sudo pip install hyperloglog pandas numpy cython patsy statsmodels matplotlib mmh3
