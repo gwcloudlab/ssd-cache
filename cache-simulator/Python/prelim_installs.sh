78f701f1ba811f29dea5d099748796a93681d1a6
Sunny Raj <sundarcs@gwu.edu>
2015-08-07 16:56:58 -0400

    Add inital dirs

--- cache-simulator/Python/prelim_installs.sh
+++ cache-simulator/Python/prelim_installs.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-sudo apt-get install -y python-dev python-pip  python-setuptools python-numpy python-scipy python-flake8 libfreetype6-dev libxft-dev libpng-dev
-sudo pip install hyperloglog pandas numpy cython patsy statsmodels matplotlib mmh3
+sudo apt-get install -y python-dev python-pip  python-setuptools python-numpy python-scipy python-flake8 libfreetype6-dev libxft-dev libpng-dev gcc-4.9 g++-4.9
+sudo pip install hyperloglog pandas numpy cython patsy statsmodels matplotlib mmh3                                                                             
+
+g++-4.9 sim_anneal.cpp -o sim_anneal
+
+mkdir -p log
+touch sa_solution.txt
