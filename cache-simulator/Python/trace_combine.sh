ca46d77334351b53297e1e06e1d849e99a732919
Sunny Raj <sundarcs@gwu.edu>
2015-08-19 11:30:38 -0400

    file not needed

--- cache-simulator/Python/trace_combine.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-python MRtoMSRformat.py sort_long_b4096.trace
-cat sort_long_b4096.csv hm.csv  > combined.csv
-sort -n combined.csv > hm_msr_all.csv
-grep -nr 848627760 hm_msr_all.csv
-head -n 1435807 hm_msr_all.csv > hm_msr_pruned.csv
