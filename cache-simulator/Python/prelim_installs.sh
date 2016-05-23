fde85879392184532da4503eb95d8a71f553f1e5
Sunny Raj <sundarcs@gwu.edu>
2016-05-23 11:52:28 -0400

    Add dependencies

--- cache-simulator/Python/prelim_installs.sh
+++ cache-simulator/Python/prelim_installs.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-sudo apt-get install -y python-dev python-pip  python-setuptools python-numpy python-scipy python-flake8 libfreetype6-dev libxft-dev libpng-dev gcc-4.9 g++-4.9
-sudo pip install hyperloglog pandas numpy cython patsy statsmodels matplotlib mmh3                                                                             
+sudo apt-get install -y python-dev python-pip  python-setuptools python-numpy python-scipy python-flake8 libfreetype6-dev libxft-dev libpng-dev gcc-6 g++-6 glib-2.0
 
-g++-4.9 sim_anneal.cpp -o sim_anneal
+sudo pip install hyperloglog pandas numpy cython patsy statsmodels matplotlib mmh3
+
+g++-6 sim_anneal.cpp -o sim_anneal
+
+cd PARDA/
+gcc -Wall -O2 `pkg-config --cflags --libs glib-2.0` -o parda.so -shared -fPIC splay.c parda.c parda_print.c narray.c -pthread -lgthread-2.0 -lglib-2.0
+cd ../
+ln -s PARDA/parda.so parda.so
 
 mkdir -p log
 touch sa_solution.txt
